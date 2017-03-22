6bc026d0c7fb58ec3b56d7ff9a9023b5ce6d205b
Dave Woodall <dave@woodalls.me>
2017-03-22 11:19:54 -0600

    wip bb

--- ff/backbone-101/js/main.js
+++ ff/backbone-101/js/main.js
@@ -1,21 +1,54 @@
-var Rectangle = Backbone.Model.extend({})
 
+var Rectangle = Backbone.Model.extend({})
 var RectangleView = Backbone.View.extend({
+
+  className: 'box',
+
+  events: {
+    "click" : function (e) {
+      this.bigBox(e);
+      this.moveBox(e);
+    }
+  },
+
   render: function() {
+    this.$el.css('display', 'block');
     this.styleBox();
-    this.$el.append(data);
     return this;
   },
 
+  bigBox: function () {
+    var width = this.model.get('width');
+    var newWidth = (width * 2);
+    this.model.set('width', newWidth);
+    this.render();
+  },
+
+  moveBox: function () {
+    this.$el.css('left', this.model.set('left') + 'px');
+  },
+
   styleBox: function() {
-      
+    this.$el.css('width', this.model.get('width') + "px");
+    this.$el.css('height', this.model.get('height') + "px");
+    this.$el.css('background', this.model.get('background'))
+    this.$el.css('position', 'absolute')
   }
-})
-
-var myRect = new Rectangle({
-
 });
-
-var myView = new RectangleView({ model: myRect })
-
+var myRect = new Rectangle({
+  width: '100',
+  height: '200',
+  left: '20',
+  background: 'blue',
+});
+var myView = new RectangleView({ el: '.box', model: myRect });
 $('#container').append(myView.render().el);
+
+
+// things to learn
+// - how to get data into model from a file.json
+// - how to update multiple views when a model changes
+// - how to manage styles in css classes
+// - how to make backbone click with clicks!
+// - how to work with templates
+// - how to work with interactive code
