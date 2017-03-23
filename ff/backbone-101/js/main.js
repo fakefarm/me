ef7df2e334139e50ee31cf6901529a33597f7911
Dave Woodall <dave@woodalls.me>
2017-03-22 21:30:40 -0600

    slowly learning backbone

--- ff/backbone-101/js/main.js
+++ ff/backbone-101/js/main.js
@@ -1,54 +1,43 @@
+var Work = Backbone.Model.extend({});
 
-var Rectangle = Backbone.Model.extend({})
-var RectangleView = Backbone.View.extend({
+var Standup = Backbone.View.extend({
+  model: Work,
 
-  className: 'box',
+  template: _.template($("#template").html()),
 
-  events: {
-    "click" : function (e) {
-      this.bigBox(e);
-      this.moveBox(e);
-    }
+  updateModel: function (kkey) {
+    attr = {}
+    return attr[kkey] = this.model.get('task')
   },
 
-  render: function() {
-    this.$el.css('display', 'block');
-    this.styleBox();
+  // _why doesn't this work?...
+  // key: this.model.get('kkey')
+  // seems that you can't tie a model in with the view's keys that are not evaluated later, by a function.
+
+
+  key: function () {
+    return this.model.get('kkey')
+  },
+
+  updateTemplate: function () {
+    var data = {};
+    data[this.key()] = this.updateModel(this.key());
+    data2 = _.extend(data, { age: 39 });
+    this.$el.html(
+      this.template(
+        data2
+      )
+    );
+  },
+
+  render: function(){
+    this.updateTemplate();
     return this;
   },
-
-  bigBox: function () {
-    var width = this.model.get('width');
-    var newWidth = (width * 2);
-    this.model.set('width', newWidth);
-    this.render();
-  },
-
-  moveBox: function () {
-    this.$el.css('left', this.model.set('left') + 'px');
-  },
-
-  styleBox: function() {
-    this.$el.css('width', this.model.get('width') + "px");
-    this.$el.css('height', this.model.get('height') + "px");
-    this.$el.css('background', this.model.get('background'))
-    this.$el.css('position', 'absolute')
-  }
 });
-var myRect = new Rectangle({
-  width: '100',
-  height: '200',
-  left: '20',
-  background: 'blue',
-});
-var myView = new RectangleView({ el: '.box', model: myRect });
-$('#container').append(myView.render().el);
 
+var work = new Work({ task: 'learn BB', kkey: 'name' });
 
-// things to learn
-// - how to get data into model from a file.json
-// - how to update multiple views when a model changes
-// - how to manage styles in css classes
-// - how to make backbone click with clicks!
-// - how to work with templates
-// - how to work with interactive code
+var standup = new Standup({ model: work });
+
+$('#container').html(standup.render().$el);
