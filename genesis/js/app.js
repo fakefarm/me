00cb5af3e9a15e6ff85b41c12abbc4e5208dd7b9
Dave Woodall <dave@woodalls.me>
2017-08-04 12:58:06 -0400

    My first commentary

---
+++ genesis/js/app.js
@@ -0,0 +1,49 @@
+var ChapterNumber = Backbone.View.extend({
+  
+  initialize: function (options) {
+    this.chapter = options['chapter'];
+    this.verse = options['verse'];
+    this.foo = options['foo'];
+    
+    this.getTemplate();
+  },
+  
+  template: 'populated on initialize',
+  
+  getTemplate: function () {
+    var self = this;
+    var template = this.foo;
+    $.get('../templates/' + template + '.html', function (data) {
+      self.template = _.template(data);
+      self.render();
+    });
+  },
+  
+  render: function () {
+    this.$el.append(this.template({chapter: this.chapter, verse: this.verse}))
+  }
+});
+
+$(function () {
+  cn = new ChapterNumber({chapter: '1', verse: '1', el: '#app', foo: 'chapter' });
+  cn2 = new ChapterNumber({chapter: '1', verse: '1', el: '#app', foo: 'verse' });
+  
+  // _dw move this into Commmentary Model
+  
+  var base = "../md/chapters/"
+  var chapter = cn.chapter
+  var verse = cn.verse
+  var end = '/commentary.md'
+  var commentary = base + chapter + '/' + verse + end
+  
+  var converter = new showdown.Converter();
+
+  $.get(commentary, function (data) {
+    html = converter.makeHtml(data);
+    $('body').append(html);
+  });
+});
+
+// pass in el
+// render
+// render with data
---
+++ genesis/md/chapters/1/1/commentary.md
@@ -0,0 +1,10 @@
+# 4 ways to read this book.
+
+There are 4 different types of people who read this verse and the rest of the bible. How you consider death will greatly influence how you read this book. Those 4 people are 4 views on what happens when we die.
+
+## 4 ways to die.
+
+## 1. Crash Test Dummy
+## 2. small gods
+## 3. biblical world view
+## 4. non-biblical world view.
