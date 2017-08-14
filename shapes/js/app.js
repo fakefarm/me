04a66b3506d99553ef0ed5e693b11b78950631c4
Dave Woodall <dave@woodalls.me>
2017-08-14 15:53:09 -0400

    unify movement variable

--- shapes/js/app.js
+++ shapes/js/app.js
@@ -1,30 +1,30 @@
 $(function () {
   $('.shape-btn').on('click', function () {
     $('.shape').toggleClass('square').toggleClass('circle');
-    $('.shape').toggleClass('left').toggleClass('right');
   });
+  var $amount = 50;
   
   $('.down-btn').on('click', function () {
     var $top = parseInt($('.shape').css('top'));
-    $top += 10;
+    $top += $amount;
     var value = $top + 'px'
     $('.shape').css('top', value);
   });
   $('.up-btn').on('click', function () {
     var $top = parseInt($('.shape').css('top'));
-    $top += -10;
+    $top += -$amount;
     var value = $top + 'px'
     $('.shape').css('top', value);
   });
   $('.right-btn').on('click', function () {
     var $left = parseInt($('.shape').css('left'));
-    $left += 10;
+    $left += $amount;
     var value = $left + 'px'
     $('.shape').css('left', value);
   });
   $('.left-btn').on('click', function () {
     var $left = parseInt($('.shape').css('left'));
-    $left += -10;
+    $left += -$amount;
     var value = $left + 'px'
     $('.shape').css('left', value);
   });
