8826a0f2f9113d2a1541f08d601bb54b6931ecc1
Dave Woodall <dave@woodalls.me>
2017-08-14 15:59:38 -0400

    add a double click

--- shapes/js/app.js
+++ shapes/js/app.js
@@ -3,6 +3,7 @@ $(function () {
     $('.shape').toggleClass('square').toggleClass('circle');
   });
   var $amount = 50;
+  var $dblclick = 300;
   
   $('.down-btn').on('click', function () {
     var $top = parseInt($('.shape').css('top'));
@@ -10,18 +11,49 @@ $(function () {
     var value = $top + 'px'
     $('.shape').css('top', value);
   });
+  
+  $('.down-btn').on('dbclick', function () {
+    var $top = parseInt($('.shape').css('top'));
+    $top += $dblclick;
+    var value = $top + 'px'
+    $('.shape').css('top', value);
+  });
+  
+  $('.up-btn').on('dblclick', function () {
+    var $top = parseInt($('.shape').css('top'));
+    $top += -$dblclick;
+    var value = $top + 'px'
+    $('.shape').css('top', value);
+  });
+  
+  $('.right-btn').on('dblclick', function () {
+    var $left = parseInt($('.shape').css('left'));
+    $left += $dblclick;
+    var value = $left + 'px'
+    $('.shape').css('left', value);
+  });
+  
+  $('.left-btn').on('dblclick', function () {
+    var $left = parseInt($('.shape').css('left'));
+    $left += -$dblclick;
+    var value = $left + 'px'
+    $('.shape').css('left', value);
+  });
+  
   $('.up-btn').on('click', function () {
     var $top = parseInt($('.shape').css('top'));
     $top += -$amount;
     var value = $top + 'px'
     $('.shape').css('top', value);
   });
+  
   $('.right-btn').on('click', function () {
     var $left = parseInt($('.shape').css('left'));
     $left += $amount;
     var value = $left + 'px'
     $('.shape').css('left', value);
   });
+  
   $('.left-btn').on('click', function () {
     var $left = parseInt($('.shape').css('left'));
     $left += -$amount;
