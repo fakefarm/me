a2b680ef71dbe2c6f903bda4280cbfdc80417f68
Dave Woodall <dave@woodalls.me>
2017-08-14 16:07:15 -0400

    refactor shapes

--- shapes/js/app.js
+++ shapes/js/app.js
@@ -1,61 +1,61 @@
 $(function () {
+  var $amount = 50;
+  var $dblclick = 300;
+  var $top;
+  var $left;
+
   $('.shape-btn').on('click', function () {
     $('.shape').toggleClass('square').toggleClass('circle');
   });
-  var $amount = 50;
-  var $dblclick = 300;
+  
+  $('.direction-btn').on('click', function () {
+    $top = parseInt($('.shape').css('top'));
+    $left = parseInt($('.shape').css('left'));
+  });
   
   $('.down-btn').on('click', function () {
-    var $top = parseInt($('.shape').css('top'));
     $top += $amount;
     var value = $top + 'px'
     $('.shape').css('top', value);
   });
   
   $('.down-btn').on('dbclick', function () {
-    var $top = parseInt($('.shape').css('top'));
     $top += $dblclick;
     var value = $top + 'px'
     $('.shape').css('top', value);
   });
   
   $('.up-btn').on('dblclick', function () {
-    var $top = parseInt($('.shape').css('top'));
     $top += -$dblclick;
     var value = $top + 'px'
     $('.shape').css('top', value);
   });
   
   $('.right-btn').on('dblclick', function () {
-    var $left = parseInt($('.shape').css('left'));
     $left += $dblclick;
     var value = $left + 'px'
     $('.shape').css('left', value);
   });
   
   $('.left-btn').on('dblclick', function () {
-    var $left = parseInt($('.shape').css('left'));
     $left += -$dblclick;
     var value = $left + 'px'
     $('.shape').css('left', value);
   });
   
   $('.up-btn').on('click', function () {
-    var $top = parseInt($('.shape').css('top'));
     $top += -$amount;
     var value = $top + 'px'
     $('.shape').css('top', value);
   });
   
   $('.right-btn').on('click', function () {
-    var $left = parseInt($('.shape').css('left'));
     $left += $amount;
     var value = $left + 'px'
     $('.shape').css('left', value);
   });
   
   $('.left-btn').on('click', function () {
-    var $left = parseInt($('.shape').css('left'));
     $left += -$amount;
     var value = $left + 'px'
     $('.shape').css('left', value);
