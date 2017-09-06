b8156b231860111667df97e83dd6107fd0d8ac00
fakefarm <dave@woodalls.me>
2017-09-06 10:50:32 -0400

    sizes as far as I could go so far

--- rp/js/vue/udemy/sizes/app.js
+++ rp/js/vue/udemy/sizes/app.js
@@ -4,17 +4,67 @@ new Vue({
     start_qty: '',
     small: '',
     medium: '',
-    large: ''
+    large: '',
+    small_adjusted: '',
+    med_adjusted: '',
+    lrg_adjusted: '',
+  },
+  computed: {
+    final_small: function() {
+      if(this.small_adjusted > 0) {
+        return parseInt(this.small_adjusted);
+      } else {
+        return parseInt(this.small);
+      }
+    },
+    final_med: function() {
+      if(this.med_adjusted > 0) {
+        return parseInt(this.med_adjusted);
+      } else {
+        return parseInt(this.medium);
+      }
+    },
+    final_lrg: function() {
+      if(this.lrg_adjusted > 0) {
+        return parseInt(this.lrg_adjusted);
+      } else {
+        return parseInt(this.large);
+      }
+    },
+    confirm_size_total: function () {
+      return this.final_lrg + this.final_small + this.final_med
+    }
   },
   methods: {
+    new_qty: function () {
+      var new_qty_var;
+      if(this.small_adjusted > 0) {
+        new_qty_var = parseInt(this.small_adjusted);
+      } else {
+        new_qty_var = parseInt(this.small);
+      }
+
+      if(this.med_adjusted > 0) {
+        new_qty_var += parseInt(this.med_adjusted);
+      } else {
+        new_qty_var += parseInt(this.medium);
+      }
+
+      if(this.lrg_adjusted > 0) {
+        new_qty_var += parseInt(this.lrg_adjusted);
+      } else {
+        new_qty_var += parseInt(this.large);
+      }
+      return new_qty_var;
+    },
     calculated_small: function() {
-      return this.start_qty / 3
+      return this.small = this.start_qty / 3
     },
     calculated_med: function() {
-      return this.start_qty / 3
+      return this.medium = this.start_qty / 3
     },
     calculated_large: function() {
-      return this.start_qty / 3
+      return this.large  = this.start_qty / 3
     },
   }
 })
