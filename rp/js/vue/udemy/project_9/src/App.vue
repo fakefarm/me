08a384a48c587a244e8a7049dd96fa1d5d849282
fakefarm <dave@woodalls.me>
2017-09-08 09:30:30 -0400

    a suboptimal solution for passing data to children

--- rp/js/vue/udemy/project_9/src/App.vue
+++ rp/js/vue/udemy/project_9/src/App.vue
@@ -2,14 +2,23 @@
   <div class="container">
     <div class="row">
       <div class="col-xs-12">
-        
+          <quote :quote='quote'></quote>
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import Quote from './components/Quote.vue'
 export default {
+  data () {
+    return {
+      quote: 'Dry details are interesting to the devoted'
+    }
+  },
+  components: {
+    Quote
+  }
 }
 </script>
 
---
+++ rp/js/vue/udemy/project_9/src/components/Quote.vue
@@ -0,0 +1,22 @@
+<template lang="html">
+  <div class="">
+    <p>{{ quote }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['quote']
+}
+</script>
+
+<style lang="css" scoped>
+  div {
+    border: 1px solid #aaa;
+    border-radius: 5px;
+    padding: 30px;
+    margin: 30px auto;
+    text-align: center;
+    font-size: 3em;
+  }
+</style>
