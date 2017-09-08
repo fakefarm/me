616a993f7cf64b4f657fe56fb7738ba922c7b3a0
fakefarm <dave@woodalls.me>
2017-09-08 10:46:59 -0400

    Use <keep-alive> for compoents to remain

--- rp/js/vue/udemy/project_9/src/App.vue
+++ rp/js/vue/udemy/project_9/src/App.vue
@@ -7,10 +7,12 @@
           <button @click="selectedComponent = 'Author'">Author</button>
           <button @click="selectedComponent = 'New'">New</button>
           <hr>
-          <component :is="selectedComponent">
-            <p>AND WE EVEN HAVE SLOTES</p>
-            <h1 slot='title'>TITLE AFTER P! WHOA! even though I wrote this after, the slot position in the child slot actually places this. that's amazing. So technically, I probably don't even need to have a h1. let me try with an h1</h1>
-          </component>
+          <keep-alive>
+            <component :is="selectedComponent">
+              <p>AND WE EVEN HAVE SLOTES</p>
+              <h1 slot='title'>TITLE AFTER P! WHOA! even though I wrote this after, the slot position in the child slot actually places this. that's amazing. So technically, I probably don't even need to have a h1. let me try with an h1</h1>
+            </component>
+          </keep-alive>
           <!-- <quote>
             coming soon.
           </quote> -->
--- rp/js/vue/udemy/project_9/src/components/New.vue
+++ rp/js/vue/udemy/project_9/src/components/New.vue
@@ -2,12 +2,19 @@
   <section>
     <div class="">
       <h2>New Quote</h2>
+      <p>Count = {{ boom }}</p>
+      <button @click='boom++'>count</button>
     </div>
   </section>
 </template>
 
 <script>
 export default {
+  data () {
+    return {
+      boom: 0
+    }
+  }
 }
 </script>
 
